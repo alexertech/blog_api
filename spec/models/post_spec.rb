4eb4db3f0b5b19a499fcda5d98e04a9bf53a2397
Alex B <ab@alexertech.com>
2023-09-27 15:42:32 -0300

    updating reading time spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'read_time calculation' do
     it 'calculates read time based on word count' do
-      body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' * 10 # 90 words
+      body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' * 100 # 900 words
       post = Post.create(title: 'Test', body: body)
 
-      # Average reading speed is 250 WPM, so 90 words should be approximately 0.36 minutes
-      expect(post.read_time).to eq(0)
+      # Average reading speed is 250 WPM, so 900 words should be approximately 3 minutes
+      expect(post.read_time).to eq(3)
     end
   end
 end
